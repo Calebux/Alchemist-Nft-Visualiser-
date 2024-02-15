f0f374f73b8c5192362d29af7558b10c1b16c6eb
calebux <petersoncaleb275@gmail.com>
2024-02-15 11:12:58 +0100

    update

--- src/ui/ShareModal.jsx
+++ src/ui/ShareModal.jsx
@@ -11,29 +11,29 @@ function ShareModal({ setIsOpen }) {
   );
   return (
     <div
-      className="absolute inset-0 flex justify-center bg-black bg-opacity-30 pt-[140px] backdrop-blur-sm"
+      className="absolute inset-0 flex justify-center px-[24px] bg-black bg-opacity-30 pt-[140px] backdrop-blur-sm"
       onClick={() => setIsOpen(false)}
     >
-      <div className="relative h-fit w-[500px] rounded-lg bg-white p-5 text-[#121212] dark:bg-black dark:text-white">
+      <div className="relative h-fit lg:w-[500px] w-full rounded-lg bg-white p-5 text-[#121212] dark:bg-black dark:text-white">
         <button
           onClick={() => setIsOpen(false)}
           className="absolute right-5 top-5 text-[28px]"
         >
           <IoIosClose />
         </button>
-        <h2 className="text-[22px] font-bold">Share</h2>
+        <h2 className="lg:text-[22px] text-base font-bold">Share</h2>
         <div className="mb-4 mt-4 flex items-center gap-x-3">
-          <div className="flex h-[56px] w-[56px] items-center justify-center rounded-full bg-[#333]">
-            <img src={x} alt="" />
+          <div className="flex lg:h-[56px] w-11 h-11 lg:w-[56px] items-center justify-center rounded-full bg-[#333]">
+            <img src={x} className="w-6 h-6 lg:h-auto lg:w-auto" alt="" />
           </div>
-          <div className="flex h-[56px] w-[56px] items-center justify-center rounded-full bg-[#8660cd]">
-            <img src={warpcast} alt="" />
+          <div className="flex lg:h-[56px] w-11 h-11 lg:w-[56px] items-center justify-center rounded-full bg-[#8660cd]">
+            <img src={warpcast} className="w-6 h-6 lg:h-auto lg:w-auto" alt="" />
           </div>
-          <div className="flex h-[56px] w-[56px] items-center justify-center rounded-full bg-[#3d4b41]">
-            <img src={hey} alt="" />
+          <div className="flex lg:h-[56px] w-11 h-11 lg:w-[56px] items-center justify-center rounded-full bg-[#3d4b41]">
+            <img src={hey} className="w-6 h-6 lg:h-auto lg:w-auto" alt="" />
           </div>
-          <div className="flex h-[56px] w-[56px] items-center justify-center rounded-full bg-[#229ed9]">
-            <img src={twitter} alt="" />
+          <div className="flex lg:h-[56px] w-11 h-11 lg:w-[56px] items-center justify-center rounded-full bg-[#229ed9]">
+            <img src={twitter} className="w-6 h-6 lg:h-auto lg:w-auto" alt="" />
           </div>
         </div>
         <div className="grid grid-cols-[auto_80px] gap-x-3">
@@ -41,10 +41,10 @@ function ShareModal({ setIsOpen }) {
             type="text"
             disabled={true}
             value={link.slice(0, 32).concat("...")}
-            className="w-full rounded-md border-[#f9f9f9] px-2 py-2 text-lg text-[#121212] disabled:cursor-not-allowed dark:text-white"
+            className="w-full rounded-md border-[#f9f9f9] px-2 py-2 text-sm lg:text-base text-[#121212] disabled:cursor-not-allowed dark:text-white"
           />
           <button
-            className="rounded bg-[#FF4B8B] px-4 py-2 font-medium text-white dark:bg-white dark:text-black"
+            className="rounded bg-[#FF4B8B] px-4 py-2 font-medium text-sm lg:text-base text-white dark:bg-white dark:text-black"
             onClick={() => {
               navigator.clipboard.writeText(link);
             }}
