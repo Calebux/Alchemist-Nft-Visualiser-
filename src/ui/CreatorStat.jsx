75f23ba68a9f371b782ea7862998e5a13b2160be
calebux <petersoncaleb275@gmail.com>
2024-02-15 11:49:12 +0100

    update

--- src/ui/CreatorStat.jsx
+++ src/ui/CreatorStat.jsx
@@ -6,7 +6,7 @@ function CreatorStat({ statName, children }) {
       <h4 className="text-[9px] text-[#3a3a3a] dark:text-white lg:text-sm">
         {statName}
       </h4>
-      <div className="flex items-center gap-x-[5px] lg:text-[10px] text-[8px] text-black dark:text-white">
+      <div className="flex items-center gap-x-[2px] lg:gap-x-[5px] lg:text-[10px] text-[8px] text-black dark:text-white">
         {children}
       </div>
     </div>
